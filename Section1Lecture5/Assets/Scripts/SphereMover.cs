ae5a33b527f02d18066c195610af391aa81a8043
Itamar Racuh <donjim@gmail.com>
2014-11-24 22:01:26 +0200

    Project containing stuff from lecture 5
    Simple sphere with rigid body and a quick c# script to match, as in the
    lecture.

---
+++ Section1Lecture5/Assets/Scripts/SphereMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMover : MonoBehaviour {
+
+	public float speed;
+	public float strafe_speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log ("speed is " + speed);
+		Debug.Log ("strafe speed is " + strafe_speed);
+	
+	}
+}
