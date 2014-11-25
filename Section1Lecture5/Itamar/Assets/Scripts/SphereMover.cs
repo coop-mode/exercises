bedda73af8769ce5d1af0169401a47cbdee05e36
Itamar Racuh <donjim@gmail.com>
2014-11-25 09:26:29 +0200

    Added user input controls, and a floor
    Controls now move the ball.
    Also, changed naming conventions in the script.  I really hate using
    underscores for variable names.

--- Section1Lecture5/Itamar/Assets/Scripts/SphereMover.cs
+++ Section1Lecture5/Itamar/Assets/Scripts/SphereMover.cs
@@ -3,19 +3,25 @@ using System.Collections;
 
 public class SphereMover : MonoBehaviour {
 
-	public float speed;
-	public float strafe_speed;
+	public float Speed;
+	public float StrafeSpeed;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		Speed = 2;
+		StrafeSpeed = 2;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-		Debug.Log ("speed is " + speed);
-		Debug.Log ("strafe speed is " + strafe_speed);
+	void Update () 
+	{
+		Debug.Log ("speed is " + Speed);
+		Debug.Log ("strafe speed is " + StrafeSpeed);
 	
+		float controlledSpeed = Input.GetAxis ("Horizontal") * Speed;
+		float controlledStrafe = Input.GetAxis ("Vertical") * StrafeSpeed;
+
+		rigidbody.AddForce (controlledSpeed, 0, controlledStrafe, ForceMode.Force);
 	}
 }
