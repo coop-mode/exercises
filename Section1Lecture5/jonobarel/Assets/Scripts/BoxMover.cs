9339f6d539f3748e82623c7adcbfd3dde5d30f3d
Jono Barel <jonobarel@gmail.com>
2014-11-30 18:25:08 +0000

    Project created
    Adding initial script for boxmover.

---
+++ Section1Lecture5/jonobarel/Assets/Scripts/BoxMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxMover : MonoBehaviour {
+
+	public float strafeSpeed = 0;
+	public float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Strafespeed = " + strafeSpeed);	
+		Debug.Log ("Speed = " + speed);
+	}
+}
